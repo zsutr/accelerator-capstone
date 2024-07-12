4d95bdffe8b126f67a26c512e354b4db7ca8225d
zsutr <zsuter21@gmail.com>
2024-07-12 11:35:50 -0400

    Express routes working for API endpoint

--- server/server.js
+++ server/server.js
@@ -27,17 +27,82 @@ app.get('/blush', async (req, res) => {
         res.status(500).send("Couldn't load products");
     }
 });
+
+app.get('/mascara', async (req, res) => {
+    try {
+       const client = await MongoClient.connect(url);
+       const db = client.db(dbName);
+       const collection = db.collection("products")
+       const mascaras = await collection.find({'product_type':'mascara'}).toArray();
+
+       res.json(mascaras);
+       console.log(mascaras);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Couldn't load products");
+    }
+});
+
+app.get('/eyebrow', async (req, res) => {
+    try {
+       const client = await MongoClient.connect(url);
+       const db = client.db(dbName);
+       const collection = db.collection("products")
+       const eyebrows = await collection.find({'product_type':'eyebrow'}).toArray();
+
+       res.json(eyebrows);
+       console.log(eyebrows);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Couldn't load products");
+    }
+});
+
+app.get('/eyeshadow', async (req, res) => {
+    try {
+       const client = await MongoClient.connect(url);
+       const db = client.db(dbName);
+       const collection = db.collection("products")
+       const eyeshadows = await collection.find({'product_type':'eyeshadow'}).toArray();
+
+       res.json(eyeshadows);
+       console.log(eyeshadows);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Couldn't load products");
+    }
+});
+
+app.get('/foundation', async (req, res) => {
+    try {
+       const client = await MongoClient.connect(url);
+       const db = client.db(dbName);
+       const collection = db.collection("products")
+       const foundations = await collection.find({'product_type':'foundation'}).toArray();
+
+       res.json(foundations);
+       console.log(foundations)
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Couldn't load products");
+    }
+});
+
+app.get('/lipstick', async (req, res) => {
+    try {
+       const client = await MongoClient.connect(url);
+       const db = client.db(dbName);
+       const collection = db.collection("products")
+       const lipsticks = await collection.find({'product_type':'lipstick'}).toArray();
+
+       res.json(lipsticks);
+       console.log(lipsticks);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Couldn't load products");
+    }
+});
  
-
-
-
-
-
-
-
-
-
-
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
